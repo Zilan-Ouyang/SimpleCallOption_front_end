c423146ae7bd126fbdc66385200fdd1d4686b04d
ZilanOuyang <zilanouyang@gmail.com>
2019-07-08 00:59:08 -0400

    Updates

---
+++ precache-manifest.854b95329a24a73428a423ecb275b975.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "852e5a539e1acead2773a7e1e9189b37",
+    "url": "/Zilan-Ouyang/SimpleCallOption_front_end/index.html"
+  },
+  {
+    "revision": "7cd9e4021c7ab31d6b27",
+    "url": "/Zilan-Ouyang/SimpleCallOption_front_end/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "e6fedd8ff85807154cdb",
+    "url": "/Zilan-Ouyang/SimpleCallOption_front_end/static/css/main.8ef63b32.chunk.css"
+  },
+  {
+    "revision": "7cd9e4021c7ab31d6b27",
+    "url": "/Zilan-Ouyang/SimpleCallOption_front_end/static/js/2.7f84fb70.chunk.js"
+  },
+  {
+    "revision": "e6fedd8ff85807154cdb",
+    "url": "/Zilan-Ouyang/SimpleCallOption_front_end/static/js/main.580213cb.chunk.js"
+  },
+  {
+    "revision": "618ddb44a6bf3b181c3c",
+    "url": "/Zilan-Ouyang/SimpleCallOption_front_end/static/js/runtime~main.649514c2.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Zilan-Ouyang/SimpleCallOption_front_end/static/media/logo.5d5d9eef.svg"
+  }
+]);
